1119366799b0f55cc02081e60c945e0e56decea8
jlashner <jlashner@gmail.com>
2015-10-09 12:51:20 -0400

    Initial Commit

---
+++ Hackathon Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hackathon Game
+//
+//  Created by Jacob Lashner on 10/9/15.
+//  Copyright © 2015 WesHack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
