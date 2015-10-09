b7032516c84ea4d1a5449785aa3dfc1bf1e0e492
jlashner <jlashner@gmail.com>
2015-10-09 13:19:14 -0400

    Base commit

--- Hackathon Game/main.cpp
+++ Hackathon Game/main.cpp
@@ -1,15 +1,32 @@
-//
-//  main.cpp
-//  Hackathon Game
-//
-//  Created by Jacob Lashner on 10/9/15.
-//  Copyright © 2015 WesHack. All rights reserved.
-//
+/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
+ and may not be redistributed without written permission.*/
 
-#include <iostream>
+//Using SDL and standard IO
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include "Engine.h"
+#include "mainmenu_state.h"
+
+int main ( int argc, char *argv[] )
+{
+	Engine game;
+    
+	// initialize the engine
+	game.Init( "Teenage Astronaut", 640, 360 , 0, false);
+    
+	// load the intro
+	game.ChangeState( MainMenuState::Instance());
+    
+	// main loop
+	while ( game.Running() )
+	{
+		game.HandleEvents();
+		game.Update();
+		game.Draw();
+        SDL_RenderPresent(game.renderer);
+	}
+    
+	// cleanup the engine
+	game.Cleanup();
+    
+	return 0;
 }
